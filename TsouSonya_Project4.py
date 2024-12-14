d0ca6e9b679ce40e87b3006f5292705545a9f26a
stsou2 <stsou2@uwo.ca>
2024-12-13 19:46:00 -0500

    Fixed all commenting and potential

--- TsouSonya_Project4.py
+++ TsouSonya_Project4.py
@@ -21,7 +21,7 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential = [], wpara
     prob (1D array): total probability computed for each timestep (conserved)
 
     '''
-    # From Lab 10:
+    # From Lab 10/11:
     def make_tridiagonal(N, b, d, a):
         '''
         Creates a tri-diagonal matrix.
@@ -76,25 +76,23 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential = [], wpara
         gaussIC = (1/(np.sqrt(sigma_0*np.sqrt(np.pi))))*(np.exp(1j*k_0*x_i))*np.exp((-(x_i-x_0)**2)/(2*sigma_0**2))
         return gaussIC
     
-    # From Lab 11
-    
-    ### parameters
+
+    # Taken from Lab 11, and textbook: Numerical Methods for Physics, Second Edition, Revised (Python) by Alejandro L. Garcia (2017)
+
+    ### Parameters
     # variable
     L = length # The system extends from x=-L/2 to x=L/2
     V = potential
-    #nspace = nspace - 1 # Technically there are nspace - 1 points to calculate;
-    # >> in light of not changing every single proceeding nspace variable I just did this to fix it
-    h = L/(nspace) # Grid spacing for periodic boundary conditions
+    h = L/(nspace) # Grid spacing for periodic boundary conditions. Textbook appears to use nspace division instead of nspace-1
     sigma0 = wparam[0]
     x0 = wparam[1]
     k0 = wparam[2]
-
     # constants
     hbar = 1
     m = 0.5
 
-    ### Create H matrix with periodic boundary conditions.
 
+    ### Create H matrix with periodic boundary conditions.
     # Tridiagonal matrix
     H = make_tridiagonal(nspace, 1, -2, 1)  
 
@@ -103,52 +101,52 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential = [], wpara
         pass
     else: # if potential has values in the array
         for v in V:
-            H[v, v] = 1* ((h**2)*2*m)/(-(hbar**2)) # accounting for coefficient; index known because on main diagonal
+            H[v, v] = -2 + 1* ((h**2)*2*m)/(-(hbar**2)) # accounting for coefficient, index known because on main diagonal
     H[0, -1] = 1   # Top right corner = b (in tridiagonal) for BC
     H[-1, 0] = 1  # Bottom left corner = a (in tridiagonal) for BC
 
-    # fixing coefficient
-    # For the given values of hbar and m, the coeff works out to just -1/h**2
-    # but I am putting the full thing in for clarity. Factored elsewhere as appropriate
+    # Fixing coefficient
+        # For the given values of hbar and m, the coeff works out to just -1/h**2
+        # but I am putting the full thing in for clarity. Factored elsewhere as appropriate
     coeff = ((-hbar**2)/(2*m*h**2))
 
-    # methods  -referring to Eqns 9.32 and 9.40 in the textbook
+
+    ### Methods, building A  -referring to Eqns 9.32 and 9.40 in the textbook
     if method == 'ftcs': # FTCS method
         A = np.eye(nspace) - (1j*tau/hbar)*coeff*H
+
+        ## Matrix stability check
+        eigenval = spectral_radius(A) # eigenvalue of largest magnitude
+        # Converting back to magnitude for stability check.
+        if abs(eigenval)-1 > 1e-10: # Unstable for magnitude > 1. Accounts for numerical error.
+            raise ValueError("FTCS solution will be unstable. Please choose a smaller timestep (tau) or the 'crank' method.")
+        else:
+            print("FTCS solution will be stable.")
     
-    elif  method == 'crank': # Crank-Nicolson method
+    elif  method == 'crank': # Crank-Nicolson method. Unconditionally stable.
         A = np.matmul(np.linalg.inv(np.eye(nspace) + ((1j*tau)/(2*hbar))*coeff*H), (np.eye(nspace) - ((1j*tau)/(2*hbar))*coeff*H))
     
     else:
         raise ValueError("Invalid method. Please enter either 'ftcs' or 'crank' as the method param.")
     
-    
-    #print(A)
-    ### Stability
-    eigenval = spectral_radius(A)
-    # Must find abs value.
-    if abs(eigenval)-1 > 1e-10:
-        print("Solution will be unstable.")
-    else:
-        print("Solution will be stable.")
-
 
     ### Solving
-         
-    # creating spatial and time grids
+    # Creating spatial and time grids
     x_grid = np.linspace(-L/2, L/2, nspace, endpoint=True)  # spatial grid from -L/2 to L/2
     t_grid = np.arange(0, ntime) * tau  # time grid 
     
     # Initializing psi
     psi = np.zeros((nspace, ntime), dtype=complex)
-    psi[:, 0] = make_gaussIC(sigma0, k0, x0, x_i=x_grid)  # IC
+    psi[:, 0] = make_gaussIC(sigma0, k0, x0, x_i=x_grid)  # initial condition for Gaussian wavepacket at psi(x, t=0)
 
+    # Iteratively solving for psi
     for n in range(1, ntime):
         psi[:,n] = np.matmul(A, psi[:,n-1])
 
-    # Finding total prob, ie psi times psi_conjugate
+    # Finding total probability
+    # Conserved to within numerical error. Should technically be equal to 1 everywhere but we have approximated constants. 
     prob = np.zeros(ntime, dtype = complex)
-    prob = np.sum(psi * np.conjugate(psi), axis=0)
+    prob = np.sum(psi * np.conjugate(psi), axis=0) # sum all rows in each column, ie. total probability at each time step.
     
     return psi, x_grid, t_grid, prob
 
@@ -159,8 +157,8 @@ def sch_plot(sch_arrays, time = 0, type = 'psi', plotshow = True, save = False):
     Plots output of sch_eqn().
 
     Args:
-    outputs (1x4 tuple): tuple of sch_eqn outputs (psi, x_grid, t_grid, prob) 
-    time (float-like): time point in grid at which plot is desired. May be approximated.
+    sch_arrays (1x4 tuple): tuple of sch_eqn outputs (psi, x_grid, t_grid, prob) 
+    time (float-like): time (s) at which plot is desired. May be approximated.
     type (str): type of plot, either 'psi' (plot of the real part of ψ(x) at time t)
         or 'prob' (plot of the particle probability density ψ ψ*(x) at a specific time). Defaults to psi
     plotshow (bool): Option to display the plot. Defaults to True
@@ -171,8 +169,13 @@ def sch_plot(sch_arrays, time = 0, type = 'psi', plotshow = True, save = False):
     t_grid = sch_arrays[2]
     prob = sch_arrays[0]*np.conjugate(sch_arrays[0])
 
-    ### Match given time ooint to closest match in time grid, rounding down 
+    ### Match given time point to closest match in time grid, rounding down 
     t_index = np.searchsorted(t_grid, time, side = 'left') # left side means t_grid[i-1] < time <= t_grid[i], ie rounding down
+    if t_index == len(t_grid):
+        t_index = len(t_grid)
+        raise ValueError("Given time outside solved range.")
+    else:
+        pass
 
     ### Plotting
     fig, ax = plt.subplots()
@@ -180,11 +183,11 @@ def sch_plot(sch_arrays, time = 0, type = 'psi', plotshow = True, save = False):
     # psi plot
     if type == 'psi':
         ax.plot(x_grid, psi[:, t_index])
-        ax.set(xlabel = 'x', ylabel = 'psi(x)', title = f'Plot of Schroedinger Wavefunction psi(x) at Time t={t_label}')
+        ax.set(xlabel = 'x', ylabel = f'psi(x, t={t_label})', title = f'Schroedinger Wavefunction Plot, t={t_label}')
     # prob plot
     elif type == 'prob':
         ax.plot(x_grid, prob[:, t_index])
-        ax.set(xlabel = 'x', ylabel = f'P(x, t={t_label})', title = f'Probability Density at Time t={t_label}')
+        ax.set(xlabel = 'x', ylabel = f'P(x, t={t_label})', title = f'Schroedinger Wavefunction Probability Density Plot, t={t_label}')
     else:
         raise ValueError("Please choose either 'psi' or 'prob' as type.")
     
@@ -202,20 +205,8 @@ def sch_plot(sch_arrays, time = 0, type = 'psi', plotshow = True, save = False):
 
     return
 
-#print(sch_eqn(nspace = 30, ntime = 500, tau = 1.0, method = 'ftcs', length = 100)[3])
-sch_plot(sch_arrays=sch_eqn(nspace = 30, ntime = 500, tau = 1.0, method = 'ftcs', length = 100), time = 10, type = 'psi')
 
 
-# fig, ax = plt.subplots()
-
-# #for i in np.linspace(0, 500, 6, endpoint = True):
-# for i in [0, 10]:
-#     psi, x_grid, t_grid, prob =sch_eqn(nspace = 100, ntime = 500, tau = 1.0, method = 'crank', length = 100)
-#     psi = np.real(psi) #taking real only
-#     ### Plotting
-#     ax.plot(x_grid, psi[:, i], label = 'i')
-
-# plt.legend()
-# plt.show()
-
-
+# Sample call
+for plott in ['psi', 'prob']:
+    sch_plot(sch_arrays=sch_eqn(nspace = 200, ntime = 500, tau = 1.0, method = 'crank', length = 200, potential = []), time = 132.678, type = plott)
